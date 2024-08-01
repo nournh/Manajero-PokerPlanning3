87a024519da1da5c3d7d8958bc33168ab8b4943e
farahbh12 <124920091+farahbh12@users.noreply.github.com>
2024-08-01 17:57:20 +0100

    commit 3

--- Back-end/src/main/java/com/example/demo/Repositories/TutorialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repositories;
-
-import com.example.demo.Entity.Tutorial;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface TutorialRepository extends MongoRepository<Tutorial, String> {
-    }
